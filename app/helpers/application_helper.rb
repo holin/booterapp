285c2340a6383cd5f2ecf858137b46cd062ba65d
HoLin <holin.he@gmail.com>
2010-08-22 17:31:22 +0800

    title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def title(name = "首页登录")
+    tl = name.gsub("'", "\\\\'")
+    content_for(:title){"#{tl} - "}
+  end
 end
